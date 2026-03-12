1558748bbf3cfc823c4a1d7be01e2ac84dccf2b0
Sylvain Girard <sylvain97@gmail.com>
2026-03-12 13:50:57 +1100

    External link detection

--- src/utils/external-link.ts
+++ src/utils/external-link.ts
@@ -4,6 +4,21 @@ const handleExternalLinks = (): void => {
   externalLinks.forEach((link) => {
     link.setAttribute('target', '_blank');
   });
+
+  const detectExternalLinks = document.querySelectorAll<HTMLAnchorElement>('a[data-detect-external]');
+  
+  detectExternalLinks.forEach((link) => {
+    const href = link.getAttribute('href');
+    // Check if it's an absolute link (starts with http:// or https://)
+    if (href && /^https?:\/\//i.test(href)) {
+      // Check if it's pointing to a different domain
+      if (link.hostname !== window.location.hostname) {
+        link.setAttribute('target', '_blank');
+        // It's good practice to add rel="noopener noreferrer" for security when opening tabs
+        link.setAttribute('rel', 'noopener noreferrer');
+      }
+    }
+  });
 };
 
 export default handleExternalLinks;
